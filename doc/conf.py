86eb824d9f3f7747ce4be19d356541a2a6204c2c
Espen Hagen <espehage@fys.uio.no>
2020-03-21 11:09:52 +0100

    test

--- doc/conf.py
+++ doc/conf.py
@@ -16,22 +16,22 @@ import sys, os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
-#sys.path.insert(0, os.path.abspath('./stubs'))
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('./stubs'))
 
 # -- Mocking Modules ---------------------------------------------------------
 
 # http://docs.readthedocs.io/en/latest/faq.html#i-get-import-errors-on-libraries-that-depend-on-c-modules
 
-#from unittest.mock import MagicMock
-#
-#class Mock(MagicMock):
-#    @classmethod
-#    def __getattr__(cls, name):
-#        return MagicMock()
-#
-#MOCK_MODULES = ['mpi4py', 'neuron']
-#sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+MOCK_MODULES = ['neuron']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
 # -- General configuration -----------------------------------------------------
