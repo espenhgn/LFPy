aea798b3e1b4c9f3b1f9eff835fcead081c4eb19
Espen Hagen <espehage@fys.uio.no>
2020-03-21 10:34:28 +0100

    test

--- doc/conf.py
+++ doc/conf.py
@@ -30,7 +30,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['mpi4py']
+MOCK_MODULES = ['mpi4py', 'neuron']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
